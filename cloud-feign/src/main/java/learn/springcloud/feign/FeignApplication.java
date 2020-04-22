1b329e4c5970402dbc7678d70bd6ca6180bda741
John Doe <johndoe@example.com>
2020-04-22 16:16:29 +0800

    no edit

--- cloud-feign/src/main/java/learn/springcloud/feign/FeignApplication.java
+++ cloud-feign/src/main/java/learn/springcloud/feign/FeignApplication.java
@@ -2,7 +2,6 @@ package learn.springcloud.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
--- cloud-feign/src/main/java/learn/springcloud/feign/consumer/service/HelloService.java
+++ cloud-feign/src/main/java/learn/springcloud/feign/consumer/service/HelloService.java
@@ -8,6 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 
 
+/**
+ * 
+ * 网上有的文章说引入的依赖是spring-cloud-starter-feign，其实官方不再推荐使用，推荐使用spring-cloud-starter-openfeign
+ * 
+ * 因为Feign中已经依赖了Hystrix，所以在Maven配置上不用做任何改动。
+ * 关于单独引入Hystrix依赖，网上有的文章说引入的依赖是spring-cloud-starter-hystrix，其实官方不在推荐使用，推荐使用spring-cloud-starter-netflix-hystrix
+ * 
+ * https://note.youdao.com/ynoteshare1/index.html?id=2f175a5a86dc34537ccf6a97ba35d1ed&type=note
+ * 
+ * @author  
+ *
+ */
 @Service
 @FeignClient(name = "hello-service666") // 调用的注册client名称
 public interface HelloService {
