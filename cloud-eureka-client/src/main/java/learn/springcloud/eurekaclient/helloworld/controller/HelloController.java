a7e820627898ea1449efc2fba3e7d00a737f5dde
John Doe <johndoe@example.com>
2020-04-22 10:39:46 +0800

    添加一个eurekaclient

---
+++ cloud-eureka-client/src/main/java/learn/springcloud/eurekaclient/helloworld/controller/HelloController.java
@@ -0,0 +1,16 @@
+package learn.springcloud.eurekaclient.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloController {
+    //private Logger logger = Logger.getLogger("HelloController");
+
+    @RequestMapping(path = "/hello2")
+    public String index(){
+      //  logger.info("this is "+request.getRequestURI());
+        return "HelloWorld666";
+    }
+}
